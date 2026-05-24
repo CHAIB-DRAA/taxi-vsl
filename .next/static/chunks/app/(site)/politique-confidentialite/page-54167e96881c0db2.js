a6700387ea626e2eab6ea68e4c00d013be608d57
chaib-draa <chaibdraa.laid@gmail.com>
2026-05-24 22:15:29 +0200

    mise a jour

---
+++ .next/static/chunks/app/(site)/politique-confidentialite/page-54167e96881c0db2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[87,160,946,428,708,899,555,529],{2247:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=2247)}),_N_E=n.O()}]);
